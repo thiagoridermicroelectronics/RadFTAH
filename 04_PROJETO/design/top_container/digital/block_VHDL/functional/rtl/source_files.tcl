8a3ce7c8c0e3c7efa4cc94b64d799aff746543f9
Thiago Rider Augusto <thiago.rider@gmail.com>
2014-09-03 20:37:11 -0300

    Succesfully get VHDL working on SimVision, Added -smartorder on irun.args and removed -exit options

---
+++ 04_PROJETO/design/top_container/digital/block_VHDL/functional/rtl/source_files.tcl
@@ -0,0 +1,6 @@
+# in this file, only the source files should be listed, NO TESTBENCH FILES!
+# a '#' character is considered a comment
+
+#../../rtl/tx_fsm.v
+#../../rtl/shift_reg_pin.v
+./trial.VHD
