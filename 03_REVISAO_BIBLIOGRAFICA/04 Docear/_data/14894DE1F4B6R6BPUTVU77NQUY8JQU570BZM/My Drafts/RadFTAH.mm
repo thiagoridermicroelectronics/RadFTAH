633ab69febeb39c891d5b087f95535ddf4273486
Thiago Rider Augusto <thiago.rider@gmail.com>
2014-11-11 00:52:46 -0200

    Modified RadFTAH mindmap

--- 03_REVISAO_BIBLIOGRAFICA/04 Docear/_data/14894DE1F4B6R6BPUTVU77NQUY8JQU570BZM/My Drafts/RadFTAH.mm
+++ 03_REVISAO_BIBLIOGRAFICA/04 Docear/_data/14894DE1F4B6R6BPUTVU77NQUY8JQU570BZM/My Drafts/RadFTAH.mm
@@ -1,6 +1,6 @@
-<map version="docear 1.1" dcr_id="1415571930717_6sfhsq4l8y1w4i8o59njrkpae" project="14894DE1F4B6R6BPUTVU77NQUY8JQU570BZM" project_last_home="file:/home/thiago/projetos/RadFTAH/03_REVISAO_BIBLIOGRAFICA/04%20Docear/">
+<map version="docear 1.1" project="14894DE1F4B6R6BPUTVU77NQUY8JQU570BZM" project_last_home="file:/home/thiago/projetos/RadFTAH/03_REVISAO_BIBLIOGRAFICA/04%20Docear/" dcr_id="1415571930717_6sfhsq4l8y1w4i8o59njrkpae">
 <!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
-<node TEXT="RadFTAH" FOLDED="false" ID="4e761ek5784lh44uptn1l38h0b" CREATED="1415571623955" MODIFIED="1415582197011" STYLE="bubble"><hook NAME="MapStyle" background="#fefefe">
+<node TEXT="RadFTAH" FOLDED="false" ID="4e761ek5784lh44uptn1l38h0b" CREATED="1415571623955" MODIFIED="1415673454809" STYLE="bubble"><hook NAME="MapStyle">
     <properties show_note_icons="true"/>
 
 <map_styles>
@@ -50,39 +50,9 @@
 </stylenode>
 </map_styles>
 </hook>
-<node POSITION="right" ID="059a37a84pukrsoq0mdefvftl9" CREATED="1415571623956" MODIFIED="1415582536812" HGAP="150" VSHIFT="-245"><richcontent TYPE="NODE">
-
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p style="font-weight: normal; background-color: #fefefe; font-style: normal" bgcolor="#fefefe">
-      Introdu&#231;&#227;o
-    </p>
-  </body>
-</html>
-
-</richcontent>
-<richcontent TYPE="DETAILS">
-
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      (At&#233; 10 p&#225;gs.)
-    </p>
-  </body>
-</html>
-
-</richcontent>
-<hook NAME="AlwaysUnfoldedNode"/>
-<hook NAME="FreeNode"/>
-<edge COLOR="#ff0000" WIDTH="3"/>
-</node>
-<node TEXT="Contexto da Pesquisa" LOCALIZED_STYLE_REF="default" POSITION="right" ID="1vhksld5j5jtr8b3fsn8r6tbb3" CREATED="1415571623956" MODIFIED="1415577218476" HGAP="80" VSHIFT="30"><richcontent TYPE="DETAILS">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
+<node TEXT="Contexto da Pesquisa" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="1vhksld5j5jtr8b3fsn8r6tbb3" CREATED="1415571623956" MODIFIED="1415674124784" HGAP="47" VSHIFT="19" BACKGROUND_COLOR="#c5c5c5"><richcontent TYPE="DETAILS">
 
 <html>
   <head>
@@ -96,18 +66,31 @@
 </html>
 
 </richcontent>
-<edge COLOR="#0000ff" WIDTH="3"/>
 <hook NAME="AlwaysUnfoldedNode"/>
-<node TEXT="O processador miniMIPS" ID="7jj54c0omm2pb25eg4i6egsass" CREATED="1415571623956" MODIFIED="1415571623956"/>
-<node TEXT="Background de toler&#xe2;ncia a falhas" ID="5d4342aml1ciuh9ardtccd73me" CREATED="1415571623956" MODIFIED="1415571623956">
-<node TEXT="Radia&#xe7;&#xe3;o e Efeitos" ID="6ag1a54t4fdk4bnvd0ja9mj7a7" CREATED="1415571623956" MODIFIED="1415571623956"/>
-<node TEXT="Eletr&#xf4;nica, SEUs e SETs" ID="0tob347c0vhd5ufimub80775e5" CREATED="1415571623956" MODIFIED="1415571623956"/>
-<node TEXT="M&#xe9;tricas para avaliar suscetibilidade dos circuitos" ID="2m5hs9o9ddv55cdhf90r2qo24u" CREATED="1415571623956" MODIFIED="1415571623956"/>
-<node TEXT="T&#xe9;cnicas de detec&#xe7;&#xe3;o de erros em processadores" ID="1k3r48rg3ikjltcd720rm42eun" CREATED="1415571623956" MODIFIED="1415571623956"/>
-<node TEXT="T&#xe9;cnicas de mitiga&#xe7;&#xe3;o para SEUs e SETs em processadores" ID="13vbecc9j6l5r3nk1eri9qekrv" CREATED="1415571623956" MODIFIED="1415571623956"/>
+<edge COLOR="#272693" WIDTH="4"/>
+<node TEXT="O processador miniMIPS" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="7jj54c0omm2pb25eg4i6egsass" CREATED="1415571623956" MODIFIED="1415674216327">
+<font BOLD="true"/>
+</node>
+<node TEXT="Background de toler&#xe2;ncia a falhas" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="5d4342aml1ciuh9ardtccd73me" CREATED="1415571623956" MODIFIED="1415674216337">
+<font BOLD="true"/>
+<node TEXT="Radia&#xe7;&#xe3;o e Efeitos" ID="6ag1a54t4fdk4bnvd0ja9mj7a7" CREATED="1415571623956" MODIFIED="1415674230000">
+<font BOLD="true"/>
+</node>
+<node TEXT="Eletr&#xf4;nica, SEUs e SETs" ID="0tob347c0vhd5ufimub80775e5" CREATED="1415571623956" MODIFIED="1415674230016">
+<font BOLD="true"/>
+</node>
+<node TEXT="M&#xe9;tricas para avaliar suscetibilidade dos circuitos" ID="2m5hs9o9ddv55cdhf90r2qo24u" CREATED="1415571623956" MODIFIED="1415674230020">
+<font BOLD="true"/>
+</node>
+<node TEXT="T&#xe9;cnicas de detec&#xe7;&#xe3;o de erros em processadores" ID="1k3r48rg3ikjltcd720rm42eun" CREATED="1415571623956" MODIFIED="1415674230023">
+<font BOLD="true"/>
+</node>
+<node TEXT="T&#xe9;cnicas de mitiga&#xe7;&#xe3;o para SEUs e SETs em processadores" ID="13vbecc9j6l5r3nk1eri9qekrv" CREATED="1415571623956" MODIFIED="1415674230026">
+<font BOLD="true"/>
 </node>
 </node>
-<node TEXT="Robustecimento de Processadores Contra Efeitos da Radia&#xe7;&#xe3;o C&#xf3;smica" POSITION="right" ID="18fl6bp21vh0ovev1hmcu3dpdc" CREATED="1415571623956" MODIFIED="1415577230789" HGAP="70" VSHIFT="40"><richcontent TYPE="DETAILS">
+</node>
+<node TEXT="Robustecimento de Processadores Contra Efeitos da Radia&#xe7;&#xe3;o C&#xf3;smica" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="18fl6bp21vh0ovev1hmcu3dpdc" CREATED="1415571623956" MODIFIED="1415674141612" HGAP="70" VSHIFT="62" BACKGROUND_COLOR="#c5c5c5"><richcontent TYPE="DETAILS">
 
 <html>
   <head>
@@ -121,20 +104,31 @@
 </html>
 
 </richcontent>
-<edge COLOR="#00ff00" WIDTH="3"/>
 <hook NAME="AlwaysUnfoldedNode"/>
-<node TEXT="Prote&#xe7;&#xf5;es em hardware" ID="2t0jtjlkcc3s9abkpvjs8e9ehn" CREATED="1415571623956" MODIFIED="1415571623956"/>
-<node TEXT="Prote&#xe7;&#xf5;es em software" ID="4i3mp3dagj59u11eru0h8vb203" CREATED="1415571623956" MODIFIED="1415571623956"/>
-<node TEXT="Fragilidades das prote&#xe7;&#xf5;es em hardware e software" ID="0aga4r4mena1vdb46l1ie725sm" CREATED="1415571623956" MODIFIED="1415571623956"/>
-<node TEXT="Arquitetura proposta" ID="7pc659g2topp9hlc7q4jq8lpsh" CREATED="1415571623956" MODIFIED="1415571623956"/>
-<node TEXT="Metodologia de verifica&#xe7;&#xe3;o" FOLDED="true" ID="0m5dlgmtf5h83opekhlr0m8mh2" CREATED="1415571623956" MODIFIED="1415571623956">
-<node TEXT="M&#xe1;quina de estados para a t&#xe9;cnica 1" ID="3kqn40bi34ss92d0db1jb3vg5r" CREATED="1415571623956" MODIFIED="1415571623956"/>
+<edge COLOR="#509b38" WIDTH="4"/>
+<node TEXT="Prote&#xe7;&#xf5;es em hardware" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="2t0jtjlkcc3s9abkpvjs8e9ehn" CREATED="1415571623956" MODIFIED="1415674216340">
+<font BOLD="true"/>
+</node>
+<node TEXT="Prote&#xe7;&#xf5;es em software" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="4i3mp3dagj59u11eru0h8vb203" CREATED="1415571623956" MODIFIED="1415674216343">
+<font BOLD="true"/>
+</node>
+<node TEXT="Fragilidades das prote&#xe7;&#xf5;es em hardware e software" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="0aga4r4mena1vdb46l1ie725sm" CREATED="1415571623956" MODIFIED="1415674216347">
+<font BOLD="true"/>
+</node>
+<node TEXT="Arquitetura proposta" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="7pc659g2topp9hlc7q4jq8lpsh" CREATED="1415571623956" MODIFIED="1415674216350">
+<font BOLD="true"/>
+</node>
+<node TEXT="Metodologia de verifica&#xe7;&#xe3;o" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="0m5dlgmtf5h83opekhlr0m8mh2" CREATED="1415571623956" MODIFIED="1415674216353">
+<font BOLD="true"/>
+<node TEXT="M&#xe1;quina de estados para a t&#xe9;cnica EDDI" ID="3kqn40bi34ss92d0db1jb3vg5r" CREATED="1415571623956" MODIFIED="1415674293376"/>
 <node TEXT="M&#xe1;quina de estados para a t&#xe9;cnica 2" ID="2h6v8ii9mdr9d6uk28fndb55f8" CREATED="1415571623956" MODIFIED="1415571623956"/>
 <node TEXT="M&#xe1;quina de estados para a t&#xe9;cnica 3" ID="5s6sbsdjbq09epaf7s14le6uea" CREATED="1415571623956" MODIFIED="1415571623956"/>
 </node>
-<node TEXT="Experimentos de inje&#xe7;&#xe3;o de falhas em simula&#xe7;&#xe3;o" ID="3easmq46b7hggkndbp7mnpvpkc" CREATED="1415571623956" MODIFIED="1415571623956"/>
+<node TEXT="Experimentos de inje&#xe7;&#xe3;o de falhas em simula&#xe7;&#xe3;o" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="3easmq46b7hggkndbp7mnpvpkc" CREATED="1415571623956" MODIFIED="1415674216356">
+<font BOLD="true"/>
 </node>
-<node TEXT="Hardware Adaptativo para Toler&#xe2;ncia a Falhas" POSITION="right" ID="0kl1rnal1074arrhj6nq40mum6" CREATED="1415571623957" MODIFIED="1415577236871" HGAP="70" VSHIFT="70"><richcontent TYPE="DETAILS">
+</node>
+<node TEXT="Hardware Adaptativo para Toler&#xe2;ncia a Falhas" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="0kl1rnal1074arrhj6nq40mum6" CREATED="1415571623957" MODIFIED="1415674144258" HGAP="70" VSHIFT="70" BACKGROUND_COLOR="#c5c5c5"><richcontent TYPE="DETAILS">
 
 <html>
   <head>
@@ -148,12 +142,16 @@
 </html>
 
 </richcontent>
-<edge COLOR="#ff00ff" WIDTH="3"/>
 <hook NAME="AlwaysUnfoldedNode"/>
-<node TEXT="Integrando as m&#xe1;quinas de estados" ID="5cakub43na2og82hosutih9f58" CREATED="1415571623957" MODIFIED="1415571623957"/>
-<node TEXT="An&#xe1;lise e experimentos de inje&#xe7;&#xe3;o de falhas usando o m&#xf3;dulo RFTAH" ID="3rm77kp43g1ts97num68unijrr" CREATED="1415571623957" MODIFIED="1415571623957"/>
+<edge COLOR="#f35e28" WIDTH="4"/>
+<node TEXT="Integrando as m&#xe1;quinas de estados" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="5cakub43na2og82hosutih9f58" CREATED="1415571623957" MODIFIED="1415674216358">
+<font BOLD="true"/>
 </node>
-<node TEXT="Conclus&#xe3;o e Trabalhos Futuros" POSITION="right" ID="7kdb5035i8nts4o0ue67pphmsu" CREATED="1415571623957" MODIFIED="1415577248942" HGAP="70" VSHIFT="20"><richcontent TYPE="DETAILS">
+<node TEXT="An&#xe1;lise e experimentos de inje&#xe7;&#xe3;o de falhas usando o m&#xf3;dulo RFTAH" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="3rm77kp43g1ts97num68unijrr" CREATED="1415571623957" MODIFIED="1415674216362">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Conclus&#xe3;o e Trabalhos Futuros" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="7kdb5035i8nts4o0ue67pphmsu" CREATED="1415571623957" MODIFIED="1415673946816" HGAP="70" VSHIFT="20" BACKGROUND_COLOR="#c5c5c5"><richcontent TYPE="DETAILS">
 
 <html>
   <head>
@@ -165,12 +163,11 @@
     </p>
   </body>
 </html>
-
 </richcontent>
-<edge COLOR="#00ffff" WIDTH="3"/>
 <hook NAME="AlwaysUnfoldedNode"/>
+<edge COLOR="#98335a" WIDTH="4"/>
 </node>
-<node TEXT="Refer&#xea;ncias" POSITION="right" ID="0g7l24vcuantacqnk0j55tnpqb" CREATED="1415571623957" MODIFIED="1415577252182" STYLE="bubble" HGAP="70" VSHIFT="30"><richcontent TYPE="DETAILS">
+<node TEXT="Refer&#xea;ncias" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="0g7l24vcuantacqnk0j55tnpqb" CREATED="1415571623957" MODIFIED="1415674146841" BACKGROUND_COLOR="#c5c5c5" HGAP="48" VSHIFT="41"><richcontent TYPE="DETAILS">
 
 <html>
   <head>
@@ -185,8 +182,9 @@
 
 </richcontent>
 <hook NAME="AlwaysUnfoldedNode"/>
+<edge COLOR="#5abdf7" WIDTH="4"/>
 </node>
-<node TEXT="Ap&#xea;ndice A: CONTRIBUI&#xc7;&#xd5;ES" POSITION="right" ID="1q8j4ujb76c6cefqf9eqmjl3fm" CREATED="1415571623957" MODIFIED="1415577255750" STYLE="bubble" HGAP="70" VSHIFT="30"><richcontent TYPE="DETAILS">
+<node TEXT="Ap&#xea;ndice A: CONTRIBUI&#xc7;&#xd5;ES" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="1q8j4ujb76c6cefqf9eqmjl3fm" CREATED="1415571623957" MODIFIED="1415674148704" BACKGROUND_COLOR="#c5c5c5" HGAP="4" VSHIFT="41"><richcontent TYPE="DETAILS">
 
 <html>
   <head>
@@ -201,6 +199,24 @@
 
 </richcontent>
 <hook NAME="AlwaysUnfoldedNode"/>
+<edge COLOR="#c74dd0" WIDTH="4"/>
+</node>
+<node TEXT="Introdu&#xe7;&#xe3;o" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_939593023" CREATED="1415674043043" MODIFIED="1415674119043" HGAP="-299" VSHIFT="-437" BACKGROUND_COLOR="#c5c5c5">
+<edge COLOR="#ff0500" WIDTH="4"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      (At&#233; 10 p&#225;gs.)
+    </p>
+  </body>
+</html>
+
+</richcontent>
 </node>
 </node>
 </map>
