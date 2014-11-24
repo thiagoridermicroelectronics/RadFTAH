90c9c5a387b0453a46d7d940cff279d401dac03e
Thiago Rider Augusto <thiago.rider@gmail.com>
2014-11-23 23:24:54 -0200

    RadFTAH Mindmap update

--- 03_REVISAO_BIBLIOGRAFICA/04 Docear/_data/14894DE1F4B6R6BPUTVU77NQUY8JQU570BZM/My Drafts/RadFTAH.mm
+++ 03_REVISAO_BIBLIOGRAFICA/04 Docear/_data/14894DE1F4B6R6BPUTVU77NQUY8JQU570BZM/My Drafts/RadFTAH.mm
@@ -1,7 +1,7 @@
 <map version="docear 1.1" dcr_id="1415571930717_6sfhsq4l8y1w4i8o59njrkpae" project="14894DE1F4B6R6BPUTVU77NQUY8JQU570BZM" project_last_home="file:/home/thiago/projetos/RadFTAH/03_REVISAO_BIBLIOGRAFICA/04%20Docear/">
 <!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
 <node TEXT="RadFTAH" FOLDED="false" ID="4e761ek5784lh44uptn1l38h0b" CREATED="1415571623955" MODIFIED="1415673454809" STYLE="bubble"><hook NAME="MapStyle">
-    <properties show_note_icons="true"/>
+    <properties show_note_icons="true" show_icon_for_attributes="true"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node">
@@ -211,10 +211,25 @@
   </body>
 </html>
 </richcontent>
-<node TEXT="As prospec&#xe7;oes do International Technology Roadmap for Semiconductors (ITRS) preveem que em pouco tempo teremos transistores de menos de uma dezena de nan&#xf4;metros e isso implica em ...." ID="ID_1431431857" CREATED="1416705507016" MODIFIED="1416776683039" HGAP="-795" VSHIFT="-120">
-<node TEXT="menor confiabilidade dos transistores" ID="ID_1788373626" CREATED="1416707362650" MODIFIED="1416776686233" HGAP="-950" VSHIFT="30"/>
-<node TEXT="manifesta&#xe7;oes de efeitos da radia&#xe7;ao ao nivel do mar" ID="ID_1057459301" CREATED="1416776690969" MODIFIED="1416776729870" HGAP="-1070" VSHIFT="30"/>
-<node TEXT="" ID="ID_924322165" CREATED="1416776740154" MODIFIED="1416776745578" HGAP="-670" VSHIFT="30"/>
+<node TEXT="As prospec&#xe7;oes do International Technology Roadmap for Semiconductors (ITRS) preveem que em pouco tempo teremos transistores de menos de uma dezena de nan&#xf4;metros, al&#xe9;m de tecnologias com m&#xfa;ltiplos gates(MGT) e isso implica em ...." ID="ID_1431431857" CREATED="1416705507016" MODIFIED="1416785471029" HGAP="-907" VSHIFT="-28">
+<node TEXT="menor confiabilidade dos transistores" ID="ID_1788373626" CREATED="1416707362650" MODIFIED="1416781545272" HGAP="-1030" VSHIFT="-27"/>
+<node TEXT="manifesta&#xe7;oes de efeitos da radia&#xe7;ao ao nivel do mar" ID="ID_1057459301" CREATED="1416776690969" MODIFIED="1416781548402" HGAP="-1150" VSHIFT="-40"/>
+<node TEXT="menores capacit&#xe2;ncias" ID="ID_924322165" CREATED="1416776740154" MODIFIED="1416781558770" HGAP="-890" VSHIFT="-30"/>
+<node TEXT="menores cargas armazenadas" ID="ID_1985803577" CREATED="1416781406948" MODIFIED="1416781564233" HGAP="-920" VSHIFT="-20"/>
+<node TEXT="menores margens de ru&#xed;do" ID="ID_1671079462" CREATED="1416781446894" MODIFIED="1416781570642" HGAP="-890" VSHIFT="-20"/>
+<node TEXT="menor consumo" ID="ID_1180288309" CREATED="1416781476807" MODIFIED="1416781567262" HGAP="-810" VSHIFT="-40"/>
+<node TEXT="Technology Scaling and Soft Error Reliability" ID="ID_1535750975" CREATED="1411247622128" MODIFIED="1416785786700" LINK="project://14894DE1F4B6R6BPUTVU77NQUY8JQU570BZM/../02_PDFs/Referencias1/06241810.pdf" HGAP="-1010" VSHIFT="-10">
+<pdf_annotation type="PDF_FILE" object_id="0" document_hash="ABAA43484F344765CCC7E7905D8FBA274B76E24BA8D4B86D2C7F43879F8F46">
+    <pdf_title>Technology Scaling and Soft Error Reliability</pdf_title>
+</pdf_annotation>
+<attribute NAME="key" VALUE="6241810"/>
+<attribute NAME="authors" VALUE="Massengill, L.W. and Bhuva, B.L. and Holman, W.T. and Alles, M.L. and Loveless, T.D."/>
+<attribute NAME="title" VALUE="Technology scaling and soft error reliability"/>
+<attribute NAME="year" VALUE="2012"/>
+</node>
+</node>
+<node TEXT="Enquanto que a tecnologia avan&#xe7;a no sentido da escalabilidade, a radia&#xe7;&#xe3;o e seus efeitos permanecem como um fen&#xf4;meno prejudicial ao correto funcionamento dos circuitos eletr&#xf4;nicos, afetando ainda mais a cada mudan&#xe7;a de n&#xf3; tecnol&#xf3;gico... O que antes era alvo de aten&#xe7;&#xe3;o quase que exclusivamente no setor aeroespacial, vem a ser considerado ao n&#xed;vel do mar." ID="ID_811508771" CREATED="1416785460394" MODIFIED="1416792170840" HGAP="-900" VSHIFT="-370">
+<node TEXT="exce&#xe7;ao: particulas alpha provenientes das impurezas dos encapsulamentos dos chips" ID="ID_1996023763" CREATED="1416792184921" MODIFIED="1416792273381" HGAP="-1236" VSHIFT="-50"/>
 </node>
 </node>
 </node>
